1f0435df7a5fcfd66aaaf8768f095df779bf6450
lncxr <lncxr@proton.me>
2025-06-19 21:15:24 +0100

    awkified build script

--- rebuild.sh
+++ rebuild.sh
@@ -3,33 +3,39 @@ set -eu
 
 FRAGMENTS_DIR="fragments"
 HTML_DIR="html"
-
-HEAD_HTML_TEMPLATE="$HTML_DIR/head.html"
-TAIL_HTML_TEMPLATE="$HTML_DIR/tail.html"
-CONTENT_HTML_TEMPLATE="$HTML_DIR/content.html"
-BOOTSTRAP_HTML_TEMPLATE="$HTML_DIR/bootstrap.html"
-
 OUTPUT_FILE="index.html"
 
-BOOTSTRAP_CDN_LINK=$(cat "$BOOTSTRAP_HTML_TEMPLATE")
-HEAD_HTML=$(sed "s|{{BOOTSTRAP_CDN_LINK}}|$BOOTSTRAP_CDN_LINK|" "$HEAD_HTML_TEMPLATE")
+awk -v bootstrap="$HTML_DIR/bootstrap.html" '
+  {
+    if (match($0, /\{\{BOOTSTRAP_CDN_LINK\}\}/)) {
+      while ((getline line < bootstrap) > 0) print line
+      close(bootstrap)
+      next
+    }
+    print
+  }
+' "$HTML_DIR/head.html" >"$OUTPUT_FILE"
 
-printf '%s\n' "$HEAD_HTML" > "$OUTPUT_FILE"
-printf '  <ul>\n' >> "$OUTPUT_FILE"
+printf '  <ul>\n' >>"$OUTPUT_FILE"
 
-for FILE in $(ls "$FRAGMENTS_DIR"/*.html | sort -r); do
-  FILENAME=$(basename "$FILE")
-  DATE=$(printf '%s' "$FILENAME" | cut -d'-' -f1-3)
-  TITLE=$(printf '%s' "$FILENAME" | cut -d'-' -f4- | sed 's/\.html$//' | tr '-' ' ')
-  CONTENT=$(cat "$FILE")
+find "$FRAGMENTS_DIR" -name '*.html' -print0 | sort -zr | while IFS= read -r -d '' FILE; do
+    FILENAME=$(basename "$FILE")
+    DATE=${FILENAME%%-*}-$(echo "$FILENAME" | cut -d'-' -f2-3)
+    TITLE=$(echo "$FILENAME" | cut -d'-' -f4- | sed 's/\.html$//' | tr '-' ' ')
 
-  CONTENT_HTML=$(cat "$CONTENT_HTML_TEMPLATE" \
-    | sed "s|{{TITLE}}|$TITLE|" \
-    | sed "s|{{DATE}}|$DATE|" \
-    | sed "s|{{CONTENT}}|$CONTENT|")
-
-  printf '%s\n' "$CONTENT_HTML" >> "$OUTPUT_FILE"
+    awk -v title="$TITLE" -v date="$DATE" -v file="$FILE" '
+    {
+      gsub(/\{\{TITLE\}\}/, title)
+      gsub(/\{\{DATE\}\}/, date)
+      if (/\{\{CONTENT\}\}/) {
+        sub(/\{\{CONTENT\}\}/, "")
+        print
+        while ((getline line < file) > 0) print line
+        close(file)
+      } else print
+    }
+  ' "$HTML_DIR/content.html" >>"$OUTPUT_FILE"
 done
 
-printf '  </ul>\n' >> "$OUTPUT_FILE"
-cat "$TAIL_HTML_TEMPLATE" >> "$OUTPUT_FILE"
+printf '  </ul>\n' >>"$OUTPUT_FILE"
+cat "$HTML_DIR/tail.html" >>"$OUTPUT_FILE"
