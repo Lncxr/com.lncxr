3bd8ac2cae00411e76ca39ced00d64b75cefd3e4
Lan <74730778+Lncxr@users.noreply.github.com>
2025-06-21 01:30:33 +0100

    decouple .html templates and dependencies
    * refactored build script, decoupled .html templates and dependencies
    
    ---------
    
    Co-authored-by: github-actions <actions@github.com>

--- rebuild.sh
+++ rebuild.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 set -eu
 
+DEPENDENCIES_DIR="dependencies"
 FRAGMENTS_DIR="fragments"
 HTML_DIR="html"
 OUTPUT_FILE="index.html"
 
-awk -v bootstrap="$HTML_DIR/bootstrap.html" '
+# Sanity check
+
+[ -d "$DEPENDENCIES_DIR" ] || { echo "Missing $DEPENDENCIES_DIR"; exit 1; }
+[ -d "$FRAGMENTS_DIR" ] || { echo "Missing $FRAGMENTS_DIR"; exit 1; }
+[ -d "$HTML_DIR" ] || { echo "Missing $HTML_DIR"; exit 1; }
+
+# Inline dependencies
+
+awk -v depdir="$DEPENDENCIES_DIR" '
   {
-    if (match($0, /\{\{BOOTSTRAP_CDN_LINK\}\}/)) {
-      while ((getline line < bootstrap) > 0) print line
-      close(bootstrap)
+    if ($0 ~ /<!-- CDN -->/) {
+      cmd = "find \"" depdir "\" -type f | sort"
+      while ((cmd | getline depfile) > 0) {
+        while ((getline dep_line < depfile) > 0) print dep_line
+        close(depfile)
+      }
+      close(cmd)
       next
     }
     print
@@ -20,8 +33,8 @@ printf '  <ul>\n' >>"$OUTPUT_FILE"
 
 find "$FRAGMENTS_DIR" -name '*.html' | sort -r | while read -r FILE; do
     FILENAME=$(basename "$FILE")
-    DATE=${FILENAME%%-*}-$(echo "$FILENAME" | cut -d'-' -f2-3)
-    TITLE=$(echo "$FILENAME" | cut -d'-' -f4- | sed 's/\.html$//' | tr '-' ' ')
+    DATE="${FILENAME%%-*}-$(echo "$FILENAME" | cut -d'-' -f2-3)"
+    TITLE="$(echo "$FILENAME" | cut -d'-' -f4- | sed 's/\.html$//' | tr '-' ' ')"
 
     awk -v title="$TITLE" -v date="$DATE" -v file="$FILE" '
     {
@@ -34,7 +47,7 @@ find "$FRAGMENTS_DIR" -name '*.html' | sort -r | while read -r FILE; do
         close(file)
       } else print
     }
-  ' "$HTML_DIR/content.html" >>"$OUTPUT_FILE"
+    ' "$HTML_DIR/content.html" >>"$OUTPUT_FILE"
 done
 
 printf '  </ul>\n' >>"$OUTPUT_FILE"
